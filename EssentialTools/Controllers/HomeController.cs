f6f554e45dd6423530bcf4d984d22d5ecd5cb2e9
Marcin Rakoczy <mrakoczy@wp.pl>
2017-11-13 11:42:53 +0100

    Update 13.11.2017

--- EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/Controllers/HomeController.cs
@@ -1,16 +1,38 @@
-﻿using System;
+﻿using EssentialTools.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ninject;
 
 namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator calc;
+
+        private Product[] products = {
+            new Product {Name = "Kajak", Category="Sporty wodne", Price = 275M},
+            new Product {Name = "Kamizelka ratunkowa", Category="Sporty wodne", Price = 48.95M},
+            new Product {Name = "Piłka nożna", Category="Piłka nożna", Price = 19.50M},
+            new Product {Name = "Flaga narożna", Category="Piłka nożna", Price = 34.95M}
+        };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
         }
 
         public ActionResult About()
